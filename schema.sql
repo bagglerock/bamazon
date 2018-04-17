9c4de85919337565790a604bf9b92be76834c409
Oscar Villalta <bagglerock@gmail.com>
2018-04-17 17:23:21 -0400

    made the schema

---
+++ schema.sql
@@ -0,0 +1,44 @@
+DROP DATABASE IF EXISTS bamazon;
+CREATE DATABASE bamazon;
+
+USE bamazon;
+
+CREATE TABLE products (
+  item_id INT NOT NULL AUTO_INCREMENT,
+  product_name VARCHAR(150) NOT NULL,
+  department_name VARCHAR(150) NULL,
+  price DECIMAL(10,4) NULL,
+  stock_quantity INT NULL,
+
+  PRIMARY KEY (item_id)
+);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("M&M's", "Candy", 2.50, 100);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Tissues", "Health & Beauty", 1.25, 25);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Legos", "Toys", 15.75, 10);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Crockpot", "Appliances", 39.99, 40);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Cotton Pillow", "Bedding", 19.99, 60);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Down Pillow", "Bedding", 129.50, 9);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("T-Shirt", "Clothing", 9.99, 50);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Jeans", "Clothing", 39.50, 30);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Bluetooth receiver", "Electronics", 49.99, 32);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("55-inch OLED TV", "Electronics", 499.99, 30);
