266d3ee2473b4a4da4ed012082e9b3fc2a630b34
Oscar Villalta <bagglerock@gmail.com>
2018-04-19 23:42:16 -0400

    added as-table

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,53 +1,59 @@
-var mysql = require("mysql");
-var inquirer = require("inquirer");
+(function runBamazon() {
+  var mysql = require("mysql");
+  var inquirer = require("inquirer");
+  var asTable = require("as-table");
 
-var connection = mysql.createConnection({
+  var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
-  
+
     // Your username
     user: "root",
-  
+
     // Your password
     password: "toto",
     database: "bamazon"
   });
 
-// function :  show all items for sale
-function showInventory(){
-    //show inventory as a list
-}
+  // function :  show all items for sale
+  function showInventory() {
+    var query = "SELECT * FROM products";
+    connection.query(query, function(err, results) {
+      if (err) throw err;
+      console.log(asTable(results));
+    });
+  }
 
-function start() {
+  function start() {
     inquirer
       .prompt({
         name: "id",
         type: "input",
-        message: "What is the ID of the item you would like to buy?",
+        message: "What is the ID of the item you would like to buy?"
       })
       .then(function(answer) {
-          var id = answer.id;
-          selectQuantity(id);
+        var id = answer.id;
+        selectQuantity(id);
       });
   }
-
-  function selectQuantity(id){
-      console.log(id);
+  
+  function checkQuantity(id) {
+    //check the availability of id and select it if available
   }
-
-  function checkQuantity(id){
-      //check the availability of id
-
+  
+  function purchase(id, quantity) {
+    //make the purchase and update quantities in the database
   }
-
-  function purchase(id, quantity){
-      //make the purchase and update quantities in the database
-  }
-
-
-
-connection.connect(function(err) {
+  
+  connection.connect(function(err) {
     if (err) throw err;
     // run the start function after the connection is made to prompt the user
-    start();
+    showInventory();
   });
+
+
+
+
+
+
+})();
